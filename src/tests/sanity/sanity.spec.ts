41940d7b1535febf47c2fe112393cd1f276b21d9
eperona <edwin.perona@gmail.com>
2025-07-20 20:10:56 +0200

    updated sanity test

--- src/tests/sanity/sanity.spec.ts
+++ src/tests/sanity/sanity.spec.ts
@@ -2,14 +2,9 @@ import {expect, test as sanity} from '../base';
 
 sanity('Sanity Test', async ({ productPage, cartPage }) => {
 
-    const isLogoVisible = await productPage.isAppLogoVisible();
-    expect(isLogoVisible).toBeTruthy();
-
     await productPage.addProductToCart('Sauce Labs Backpack');
     await productPage.addProductToCart('Sauce Labs Bike Light');
-
     await productPage.clickCartIcon();
-
     expect(await cartPage.getCartItemsCount()).toBe(2);
 
 });
